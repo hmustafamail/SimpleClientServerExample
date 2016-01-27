4ebdeee74a3aecc62da5b8e53ccfde9a5002866e
MUSTAFA <h.mustafa.mail@gmail.com>
2016-01-27 13:51:12 -0500

    Added files

---
+++ README.txt
@@ -0,0 +1,21 @@
+In Windows:
+
+1. Ensure you have Python 2.7.* installed.
+
+2. Open two command line windows here
+    a. SHIFT-right-click in the file browser window
+    b. Select "Open command window here"
+    c. Repeat one more time to open a second window
+    
+3. Type the following commands, one into each window:
+    a. python server1.py
+    b. python client1.py hello world
+    
+4. The server window should say:
+
+        hello world
+
+    And the client window should say:
+
+        Sent: hello world
+        Received: HELLO WORLD
---
+++ client1.py
@@ -0,0 +1,43 @@
+# A simple client example
+# Mustafa Hussain (TA) For Networks, Dr. Dean Bushey, Spring 2016, FL Poly
+
+import socket
+import sys
+
+# We will write to the server at their address.
+HOST = "localhost"
+
+# We also need to know their "apartment number" to get our message to them.
+PORT = 9999
+
+# This gets all the stuff on the command line after the name of the program.
+userWords = sys.argv[1:]
+
+# The stuff comes in as a list. Let's join it all together to get a big string.
+data = " ".join(userWords)
+
+# Create a socket (SOCK_STREAM for a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# This means "Do this, knowing it might fail..."
+try:
+    # Connect to server.
+    sock.connect((HOST, PORT))
+    
+    # Send our message to the server. 
+    # '\n' is known as the newline character.
+    # It's what happens when you hit the ENTER key in a text editor.
+    sock.sendall(data + "\n")
+
+    # Receive the message from the server.
+    received = sock.recv(1024)
+    
+# "...and whether or not it fails, do this."
+finally:
+    sock.close()
+
+# This shows the user what we sent to the sever...
+print "Sent:     {}".format(data)
+
+# ...and what we got back from the server.
+print "Received: {}".format(received)
